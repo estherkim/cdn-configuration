1cb11161ff208828323ee83dabde8c1219f38584
estherkim <esth@google.com>
2022-03-01 13:12:50 -0500

    prettier

--- scripts/check-qa.ts
+++ scripts/check-qa.ts
@@ -11,26 +11,33 @@ const octokit = new Octokit({
   auth: process.env.ACCESS_TOKEN,
 });
 const pullNumber = githubRef.search(/(\d+)/g);
-const params = {owner: 'estherkim', repo: 'cdn-configuration', pull_number: pullNumber };
+const params = {
+  owner: 'estherkim',
+  repo: 'cdn-configuration',
+  pull_number: pullNumber,
+};
 
 async function main() {
-  const {data: pull} = await octokit.rest.pulls.get(params)
+  const {data: pull} = await octokit.rest.pulls.get(params);
   if (!pull.head.ref.startsWith('') || !pull.head.ref.startsWith('')) {
     // skip for other branches
     return;
   }
 
   // get list of approvers
-  const {data: reviews } = await octokit.rest.pulls.listReviews(params);
-  const approvers = reviews.map(review => {
+  const {data: reviews} = await octokit.rest.pulls.listReviews(params);
+  const approvers = reviews.map((review) => {
     if (review && review.state == 'approved' && review.user) {
       return review.user.login;
     }
   });
 
   // check if at least 1 approver is qa
-  const {data: qas} = await octokit.teams.listMembersInOrg({org: 'ampproject', team_slug: 'amp-qa'});
-  const approverIsQa = qas.some(qa => approvers.includes(qa.login));
+  const {data: qas} = await octokit.teams.listMembersInOrg({
+    org: 'ampproject',
+    team_slug: 'amp-qa',
+  });
+  const approverIsQa = qas.some((qa) => approvers.includes(qa.login));
   if (approverIsQa) return;
 
   console.error('This pull request is missing an approval from QA');
